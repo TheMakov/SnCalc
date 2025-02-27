4af8586c8c91ba7045289d7c239af7ce1b7dc4c6
Mischa Prudnikov <mishaprudnikov11@gmail.com>
2025-02-27 20:28:43 +0100

    actual intial commit, configured the library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ButtonModule} from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ButtonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'sn-calc';
