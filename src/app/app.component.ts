365611494cc9b56591983e4c9aa551b816dc6f06
Mischa Prudnikov <mishaprudnikov11@gmail.com>
2025-02-28 22:42:27 +0100

    sidebar prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,33 @@ import {FloatLabel} from 'primeng/floatlabel';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {trigger, state, style, transition, animate} from '@angular/animations';
+import {NgIf} from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  imports: [ ButtonModule, RouterOutlet, FormsModule, FloatLabel, InputText],
+  imports: [ButtonModule, RouterOutlet, FormsModule, FloatLabel, InputText, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+
   animations: [
-    trigger('openClose', [state('closed', style({marginLeft: '0vw'}))])
+    trigger('toggle_sidebar', [
+      transition(':leave', [  // When the element appears
+        animate('500ms ease-in-out', style({ marginLeft: '-24.5vw' }))
+      ]),
+      transition(':enter', [  // When the element disappears
+        style({  marginLeft: '-25vw' }),  // Ensure position is set
+        animate('500ms ease-in-out', style({ marginLeft: '0vw' }))
+      ])
+    ])
   ]
 })
 export class AppComponent {
   title = 'sn-calc';
   value: any;
+  sidebar = false;
+
+  toggleSidebar() {
+    this.sidebar = !this.sidebar;
+  }
 }
