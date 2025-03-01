561d56e1f13a4c36c8804d5751976ec4596d924c
Mischa Prudnikov <mishaprudnikov11@gmail.com>
2025-03-01 12:32:59 +0100

    made sidebar better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ButtonModule} from 'primeng/button';
-import {InputText, InputTextModule} from 'primeng/inputtext';
+import {InputText} from 'primeng/inputtext';
 import {FormsModule} from '@angular/forms';
 import {FloatLabel} from 'primeng/floatlabel';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {trigger, state, style, transition, animate} from '@angular/animations';
 import {NgIf} from '@angular/common';
 
@@ -19,7 +17,7 @@ import {NgIf} from '@angular/common';
   animations: [
     trigger('toggle_sidebar', [
       transition(':leave', [  // When the element appears
-        animate('500ms ease-in-out', style({ marginLeft: '-24.5vw' }))
+        animate('500ms ease-in-out', style({ marginLeft: '-25vw' }))
       ]),
       transition(':enter', [  // When the element disappears
         style({  marginLeft: '-25vw' }),  // Ensure position is set
---
+++ src/app/variable-box/variable-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-variable-box',
+  imports: [],
+  templateUrl: './variable-box.component.html',
+  styleUrl: './variable-box.component.css'
+})
+export class VariableBoxComponent {
+
+}
