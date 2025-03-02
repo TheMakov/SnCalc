eca3d3c8ea97ae08f09c5d708313c5c7abecd3e1
Mischa Prudnikov <mishaprudnikov11@gmail.com>
2025-03-02 18:47:00 +0100

    prototyping for matrix

--- src/app/variable-box/variable-box.component.ts
+++ src/app/variable-box/variable-box.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component} from '@angular/core';
 import {FloatLabel} from 'primeng/floatlabel';
 import {InputText} from 'primeng/inputtext';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {Button} from 'primeng/button';
 import {ToggleButton} from 'primeng/togglebutton';
+import {NgClass, NgForOf, NgIf} from '@angular/common';
+import {ScrollPanel} from 'primeng/scrollpanel';
+import {Scroller} from 'primeng/scroller';
+import {InputOtp} from 'primeng/inputotp';
 
 
 @Component({
@@ -14,7 +18,10 @@ import {ToggleButton} from 'primeng/togglebutton';
     ReactiveFormsModule,
     FormsModule,
     ToggleButton,
-    Button
+    Button,
+    NgIf,
+    InputOtp,
+    NgForOf,
   ],
   templateUrl: './variable-box.component.html',
   styleUrl: './variable-box.component.css'
@@ -22,6 +29,28 @@ import {ToggleButton} from 'primeng/togglebutton';
 export class VariableBoxComponent {
   name: any;
   value: any;
-  checked: any;
+  checked: any = true;
 
+  matrix_upper_row_value_string:any  = ['1', '2'];
+  matrix_value: string = "";
+  matrix_length: any = 2;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+
+  update_otp_length (){
+    let old_value_length = this.matrix_value.length;
+    let old_matrix_length = this.matrix_length;
+    this.matrix_length = this.matrix_value.length +2;
+    if(old_matrix_length >= old_value_length+2){
+      this.matrix_upper_row_value_string.pop()
+      console.log(this.matrix_upper_row_value_string);
+    }
+    else{
+      this.matrix_upper_row_value_string.push((old_value_length +2).toString());
+      console.log(this.matrix_upper_row_value_string);
+    }
+
+
+  }
 }
