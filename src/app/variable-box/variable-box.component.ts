e711fb03b5ebf71f6d719b09d1ee577c08b45b37
Mihail Prudnikov <mishaprudnikov11@gmail.com>
2025-03-16 21:19:14 +0100

    made a part of the column bs work

--- src/app/variable-box/variable-box.component.ts
+++ src/app/variable-box/variable-box.component.ts
@@ -6,6 +6,7 @@ import {Button} from 'primeng/button';
 import {ToggleButton} from 'primeng/togglebutton';
 import {NgForOf, NgIf} from '@angular/common';
 import {TableModule} from 'primeng/table';
+import {min} from 'rxjs';
 
 
 @Component({
@@ -30,7 +31,6 @@ export class VariableBoxComponent {
   checked: any = true;
   valid : boolean = true;
 
-
   columns = [0];  // Starts with 1 column
   tableData: number [][] = [
     [1],  // First row
@@ -42,9 +42,62 @@ export class VariableBoxComponent {
   onFinishedEdit(rowIndex: number, colIndex: number){
     this.valid = this.matrixIsValid();
     if(this.tableData[0][colIndex] == null && this.tableData[1][colIndex] == null){
-      this.removeColumn(colIndex);
+     // this.removeColumn(colIndex);
     }
   }
+
+  checkMatrixLength(){
+    //save columns length before modifying it
+    const columnsCount = this.columns.length
+
+
+    if((this.tableData[0][columnsCount-1] !== null && this.tableData[0][columnsCount-1] !== undefined) || (this.tableData[1][columnsCount-1] !== null && this.tableData[1][columnsCount-1] !== undefined)){
+      this.columns.push(this.columns.length);
+    }
+    else if((this.tableData[0][columnsCount-2] == null ||this.tableData[0][columnsCount-2] == undefined) && (this.tableData[1][columnsCount-2] == null ||this.tableData[1][columnsCount-2] == undefined)){
+      this.columns.pop();
+    }
+
+    console.log((this.tableData[0][this.columns.length-1] !== null && this.tableData[0][this.columns.length-1] !== undefined));
+    console.log((this.tableData[0][this.columns.length-1]))
+    console.log(this.tableData)
+  }
+
+  onInput(){
+    this.checkMatrixLength()
+  }
+
+
+
+  OnFocus(){
+    this.checkMatrixLength()
+  }
+
+  OnKeyEvent(event:KeyboardEvent, rowIndex:number, colIndex: number ){
+    const table = document.querySelector("p-table"); // Get the table element
+    const inputs = table?.querySelectorAll("input");
+
+
+    if (!inputs) return
+    let nextIndex = -1;
+
+    if(event.key == "ArrowRight" || event.key == "D"){
+      nextIndex = colIndex + 1; // Move right
+    }
+    else if(event.key == "ArrowLeft" || event.key == "A"){
+      nextIndex = colIndex - 1;
+    }
+
+    if (nextIndex >= 0 && nextIndex < inputs.length) {
+      event.preventDefault(); // Prevent cursor movement
+      (inputs[nextIndex] as HTMLElement).focus(); // Move focus
+    }
+  }
+
+  addColumn(){
+
+  }
+
   matrixIsValid(): boolean {
     return !(this.hasDuplicates(this.tableData[0]) || this.hasDuplicates(this.tableData[1]));
   }
